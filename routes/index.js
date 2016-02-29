e95729f5fcf5921046750126e600ba5930e17a51
Sharon Rumsey <sharon@rumsey.nz>
2016-02-29 20:31:16 +1300

    jade and bootstrap

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Ternary is Beautiful Code' });
 });
 
 module.exports = router;
